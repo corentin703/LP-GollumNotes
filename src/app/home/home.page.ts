164e17ebca9cffbf32a94b4b9d7898a1e19c8039
Corentin VÉROT <corentin.verot@outlook.com>
2022-05-31 23:10:50 +0200

    Ajout bouton prise photo (fonctionnel)

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {PhotoService} from '../services/photo.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePage implements OnInit {
 
-  constructor() { }
+  constructor(
+    public photoService: PhotoService
+  ) { }
 
   ngOnInit() {
   }
 
+  addPhotoToGallery() {
+    this.photoService.addNewToGallery();
+  }
 }
