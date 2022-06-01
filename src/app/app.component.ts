27fe2847a16afe32729ee076bc8aa5791eaacb13
volinico <nicolas.morin@etu.uca.fr>
2022-06-01 19:31:11 +0200

    debut du visuel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,14 +6,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public appPages = [
-    { title: 'Accueil', url: '/home', icon: 'home' },
-    { title: 'Inbox', url: '/folder/Inbox', icon: 'mail' },
-    { title: 'Outbox', url: '/folder/Outbox', icon: 'paper-plane' },
-    { title: 'Favorites', url: '/folder/Favorites', icon: 'heart' },
-    { title: 'Archived', url: '/folder/Archived', icon: 'archive' },
-    { title: 'Trash', url: '/folder/Trash', icon: 'trash' },
-    { title: 'Spam', url: '/folder/Spam', icon: 'warning' },
+    { title: 'Notes', url: '/notes', icon: 'home' },
+    { title: 'Rappels', url: '/rappels', icon: 'bulb' },
+    { title: 'Archive', url: '/archive', icon:'archive' },
+    { title: 'Corbeille', url: '/corbeille', icon: 'trash' },
+    // { title: 'Inbox', url: '/folder/Inbox', icon: 'mail' },
+    // { title: 'Outbox', url: '/folder/Outbox', icon: 'paper-plane' },
+    // { title: 'Favorites', url: '/folder/Favorites', icon: 'heart' },
+    // { title: 'Archived', url: '/folder/Archived', icon: 'archive' },
+    // { title: 'Trash', url: '/folder/Trash', icon: 'trash' },
+    // { title: 'Spam', url: '/folder/Spam', icon: 'warning' },
   ];
-  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
+      // <ion-icon name="document-outline"></ion-icon>
+  // public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
   constructor() {}
 }
