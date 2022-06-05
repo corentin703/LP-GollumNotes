c8850917897327dd6a744b4d9fafddb0e09cb0ab
volinico <nicolas.morin@etu.uca.fr>
2022-06-05 17:51:31 +0200

    ajout d'une petite partie visuel pour les notes ajoutées

--- src/app/note/note.component.ts
+++ src/app/note/note.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
-import {Note } from '../@entities/Note';
-import {createNote } from '../@entities/Note';
+import {Note} from '../services/http/note.service.type';
+// import {Note } from '../@entities/Note';
+// import {createNote } from '../@entities/Note';
 ////
 //import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 // import { CustomComponent } from './custom.component';
@@ -14,12 +15,20 @@ export class NoteComponent implements OnInit {
 
   @Input() note: Note
 
+  title
+  content
+
   constructor() {
   // this.note = createNote("idstring", "descriptionstring")
+
+
  }
 
   ngOnInit() {
-    // console.log("test")
+    this.title = this.note.title
+    this.content = this.note.content
+    console.log(this.note.id)
+    // content =""
   }
 
 
--- src/app/notes/notes.page.ts
+++ src/app/notes/notes.page.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
-import {Note } from '../@entities/Note';
+// import {Note } from '../@entities/Note';
 import {Profile,createProfile } from '../@entities/Profile';
 import {createNote} from '../@entities/Note';
 import {NoteService} from '../services/http/note.service';
 import {AccountService} from '../services/http/account.service';
+import {Note, CreateNoteRequest} from '../services/http/note.service.type';
 
 @Component({
   selector: 'app-notes',
@@ -43,11 +44,48 @@ export class NotesPage implements OnInit {
     //   this.notes = response;
     // })
 
+    // let newNote: CreateNoteRequest = { title: "titileTest", content: "contentTest" };
+
+
     this.accountService.login('identifiant2Profile', 'passwordProfile').subscribe(
       loginRes => {
         console.log('retour login', loginRes);
       }
     );
+
+    // this.noteService.create(newNote).subscribe(
+    //   createRes => {
+    //     console.log('retour addNote', createRes);
+    //   }
+    // );
+
+    this.noteService.getAll().subscribe(
+      getAllRes => {
+        console.log('retour getAllNote', getAllRes);
+        console.log('retour getAllNote', getAllRes.data);
+
+        getAllRes.data.forEach(element => {
+          let note: Note = {
+            id: element.id,
+            title: element.title,
+            content: element.content,
+            createdAt: element.createdAt,
+            lastModifiedAt: element.lastModifiedAt,
+            pictures: element.pictures
+          }
+          this.notes.push(note);
+          
+
+          console.log("title " + element.title);
+
+          // this.notes.push(element as Note)
+        });
+        // this.notes = getAllRes.data as Note[]
+      }
+    );
+
+    console.log("nb notes " + this.notes.length);
+
 
     // this.accountService.register('identifiant2Profile', 'passwordProfile').subscribe(
     //   result => {
